cc5abe23a9f35521cd81fc78d1388b2c58c54b01
Joseph Sofia <jsofia@stevens.edu>
2023-11-28 13:27:14 -0500

    use Azure's 8080 port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8080;
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
